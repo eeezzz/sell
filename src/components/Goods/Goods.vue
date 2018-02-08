8b82fe22643451ea813c19fa73095108662c3cd9
giming <giming@hsin-yi.org.tw>
2018-02-08 17:39:16 +0800

    add shopcart

--- src/components/Goods/Goods.vue
+++ src/components/Goods/Goods.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="goods">
     <!-- 分類 -->
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuScroll">
       <ul>
         <!-- 專場 -->
-        <li class="menu-item">
+        <li class="menu-item" :class="{'current':currentIndex===0}" @click="selectMenu(0)">
           <p class="text">
             <img :src="container.tag_icon" v-if="container.tag_icon" class="icon" />
             {{container.tag_name}}
           </p>
         </li>
 
-        <li class="menu-item" v-for="(item, index) in goods" :key="index">
+        <li class="menu-item" v-for="(item, index) in goods" :key="index" :class="{'current':currentIndex===index+1}" @click="selectMenu(index+1)">
           <p class="text">
             <img :src="item.icon" v-if="item.icon" class="icon" />
             {{item.name}}
@@ -21,16 +21,16 @@
 
     </div>
     <!-- 商品列表 -->
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodScroll">
       <ul>
         <!-- 專場 -->
-        <li class="container-list">
+        <li class="container-list food-list-hook">
           <div v-for="(item,index) in container.operation_source_list" :key="index">
             <img :src="item.pic_url">
           </div>
         </li>
         <!-- 具體分類 -->
-        <li v-for="(item, index) in goods" :key="index" class="food-list">
+        <li v-for="(item, index) in goods" :key="index" class="food-list food-list-hook">
           <h3 class="title">{{item.name}}</h3>
 
           <!-- 具體商品列表 -->
@@ -57,23 +57,69 @@
       </ul>
 
     </div>
+
+    <shopcart></shopcart>
   </div>
 </template>
 
 <script>
+// 導入BScroll
+import BScroll from 'better-scroll'
+import Shopcart from '../shopcart/shopcart'
+
 export default {
   data(){
     return {
       container: {},
       goods: [],
+      listHeight: [],
+      scrollY: 0,
+      menuScroll: {},
+      foodScroll: {},
     }
   },
   methods:{
     head_bg(picture){
       // console.log(picture)
       return "background-image: url("+ picture + ")"
-    }
+    },
+    initScroll(){
+      
+      this.menuScroll = new BScroll(this.$refs.menuScroll, { click: true })
+      this.foodScroll = new BScroll(this.$refs.foodScroll, { probeType: 3 })
+
+      // 添加滾動監聽事件
+      this.foodScroll.on('scroll', (pos) => {
+        this.scrollY = Math.abs(Math.round(pos.y))
+        console.log(this.scrollY)
+      })
+    },
+    calculateHeight(){
+      // 通過refs獲取到對應的元素
+      let foodlist = this.$refs.foodScroll.getElementsByClassName('food-list-hook')
+      // 添加到數組區間
+      let height = 0;
+      this.listHeight.push(height)
+      for(let i=0; i<foodlist.length; i++)
+      {
+        let item = foodlist[i]
+        // 高度需累加
+        height += item.clientHeight
+        this.listHeight.push(height)
+
+      }
+    },
+    selectMenu(index){
+      console.log('selectIndex->',index)
+      // 通過refs獲取到對應的元素
+      let foodlist = this.$refs.foodScroll.getElementsByClassName('food-list-hook')
+      // 根據下標，滾動到相對應的元素
+      let el = foodlist[index]
+      this.foodScroll.scrollToElement(el, 250)
+
+    },
   },
+
   created(){
     let _this = this
     // 發起異步請求，獲取數據
@@ -86,13 +132,44 @@ export default {
           _this.goods = dataSource.data.food_spu_tags
           console.log(_this.container)
           console.log(_this.goods)
+         
+          // 目前Dom元素並沒有被渲染（即高度是問題）
+          // 調用$nextTick後，Dom已經更新
+          // 這時再調用滾動的初始方法
+          _this.$nextTick(()=>{
+            _this.initScroll()
+            // 計算分類區間高度
+            _this.calculateHeight()
+          })
         }
       })
       .catch((error)=>{
         console.log(error)
       })
 
-  }
+
+  },
+  components: {
+    Shopcart,
+  },
+  computed: {
+    // 根據右側的滾動位置，確定對應的索引下標
+    currentIndex(){
+      for(let i=0; i<this.listHeight.length; i++){
+        // 取得商品區範圍
+        let height1 = this.listHeight[i]
+        let height2 = this.listHeight[i+1]
+
+        // 判斷是否在區間中
+        if (this.scrollY>=height1 && this.scrollY<height2){
+          console.log('currentIndex=>',i)
+          return i
+        }
+      }
+      return 0
+    }
+  },
+
 }
 </script>
 
---
+++ src/components/shopcart/shopcart.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="shopcart">
+    <div class="content-left">
+      <div class="logo-wrapper">
+        <span class="icon-shopping_cart"></span>
+      </div>
+      <div class="desc-wrapper">
+        <p class="tip">另需配送用￥9</p>
+      </div>
+    </div>
+    <div class="content-right">
+      ￥0起送
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+@import url('shopcart.css');
+</style>
